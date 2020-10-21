6da5f5ed0f824e6a0d8859f37e575364f099814c
Cameron-Jade Mummery <cami.mummery@aexp.com>
2020-10-21 15:19:45 +0100

    linking frontend to backend

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -2,12 +2,13 @@ package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
-    String home() {
-        return "Hello World";
+    ModelAndView home() {
+        return new ModelAndView("hello");
     }
 }
